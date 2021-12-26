33c97bc95770a7b9bca0fe8bd4462be8cf06b638
just-be-weird <abhi.chandresh@gmail.com>
2021-12-26 14:13:44 +0530

    setup

--- playground/arrays.js
+++ playground/arrays.js
@@ -2,21 +2,20 @@
  * Objects and arrays are reference type
  * here values are stored in memory and variable
  * to whom these values are assigned is pointer
- * to memory loaction i.e. 'person' is reference
+ * to memory location i.e. 'person' is reference
  * pointer
- * 
+ *
  */
 
- const person = {
-     name: 'jonas'
- };
+const person = {
+  name: "jonas",
+};
 
- const someone = person;//created deep copy
- const someone2 = {...person}//creates shallow copy by using spread operator
+const someone = person; //created deep copy
+const someone2 = { ...person }; //creates shallow copy by using spread operator
 
- console.log(someone.name);
- 
+console.log(someone.name);
 
- // array functions
+// array functions
 
- const arr = [1,2,3,4,5,6];
+const arr = [1, 2, 3, 4, 5, 6];
